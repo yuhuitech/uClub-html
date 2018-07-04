3d3d287968f5b07cdd627001eeada3c46a62fa16
覃柳悦-070 <1435238295@qq.com>
2018-07-04 09:10:16 +0800

    登陆功能

---
+++ src/main/java/model/Club.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Club {
+    private int ClubNo;
+    private String ClubName;
+    private String Type;
+    private String ClubInfo;
+    private String Res_url;
+
+    public int getClubNo() {
+        return ClubNo;
+    }
+
+    public void setClubNo(int clubNo) {
+        ClubNo = clubNo;
+    }
+
+    public String getClubName() {
+        return ClubName;
+    }
+
+    public void setClubName(String clubName) {
+        ClubName = clubName;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getClubInfo() {
+        return ClubInfo;
+    }
+
+    public void setClubInfo(String clubInfo) {
+        ClubInfo = clubInfo;
+    }
+
+    public String getRes_url() {
+        return Res_url;
+    }
+
+    public void setRes_url(String res_url) {
+        Res_url = res_url;
+    }
+}
